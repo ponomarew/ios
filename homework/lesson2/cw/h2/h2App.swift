441da90323e98a8b883e69ef7e7857c07d68f453
TiiviuCW <136173794+TiiviuCW@users.noreply.github.com>
2025-11-28 22:48:06 +0700

    hw2 (#4)
    * hw1
    
    * code review fixes
    
    * hw2
    
    ---------
    
    Co-authored-by: Vy <pumpkin171@icloud.com>

---
+++ homework/lesson2/cw/h2/h2App.swift
@@ -0,0 +1,17 @@
+//
+//  h2App.swift
+//  h2
+//
+//  Created by Tiiviu on 28/11/25.
+//
+
+import SwiftUI
+
+@main
+struct h2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
